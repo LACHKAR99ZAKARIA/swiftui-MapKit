5f61d48a4bc87c2ff0b239386b2043b25dc983e7
LACHKAR99ZAKARIA <lachkar99zaakria@gmail.com>
2023-06-19 09:29:58 +0100

    Initial Commit

---
+++ SwiftUI-Map/SwiftUI_MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MapApp.swift
+//  SwiftUI-Map
+//
+//  Created by Zakarai Lachkar on 19/6/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
