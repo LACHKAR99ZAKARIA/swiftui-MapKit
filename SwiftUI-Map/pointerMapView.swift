89ae6eca1779e62a2aefaee086b933fab7d941c9
LACHKAR99ZAKARIA <lachkar99zaakria@gmail.com>
2023-06-19 10:39:47 +0100

    first commit

---
+++ SwiftUI-Map/pointerMapView.swift
@@ -0,0 +1,20 @@
+//
+//  pointerMapView.swift
+//  SwiftUI-Map
+//
+//  Created by Zakarai Lachkar on 19/6/2023.
+//
+
+import SwiftUI
+
+struct pointerMapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct pointerMapView_Previews: PreviewProvider {
+    static var previews: some View {
+        pointerMapView()
+    }
+}
